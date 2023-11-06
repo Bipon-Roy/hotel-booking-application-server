a011d06bdfd58bb39e10aa701a1edd21c05a30cc
Bipon Roy <biponroy400@gmail.com>
2023-11-06 14:31:22 +0600

    Added JWT Authentication functionality

--- index.js
+++ index.js
@@ -4,10 +4,18 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 
 // middleware
-app.use(cors());
+app.use(
+    cors({
+        origin: ["http://localhost:5173"],
+        credentials: true,
+    })
+);
 app.use(express.json());
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Pass}@cluster0.sw3jgjt.mongodb.net/?retryWrites=true&w=majority`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -19,6 +27,11 @@ const client = new MongoClient(uri, {
     },
 });
 
+const logger = (req, res, next) => {
+    console.log("log: info", req.method, req.url);
+    next();
+};
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -26,6 +39,23 @@ async function run() {
 
         const roomCollection = client.db("hotelDb").collection("rooms");
         //auth related api
+        app.post("/jwt", logger, async (req, res) => {
+            const user = req.body;
+            console.log("user token", user);
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1h" });
+            res.cookie("token", token, {
+                httpOnly: false,
+                secure: true,
+                sameSite: "none",
+                // secure: process.env.NODE_ENV === "production",
+                // sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+            }).send({ success: true });
+        });
+        app.post("/logout", async (req, res) => {
+            const user = req.body;
+            console.log("logging out", user);
+            res.clearCookie("token", { maxAge: 0 }).send({ success: true });
+        });
 
         //Hotels related api
         app.get("/rooms", async (req, res) => {
