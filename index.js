7eb94594990ad35d019de30d3e772199a6b686f1
Bipon Roy <biponroy400@gmail.com>
2023-11-08 18:42:46 +0600

    Added vercel config file

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ const cookieParser = require("cookie-parser");
 // middleware
 app.use(
     cors({
-        origin: ["http://localhost:5173"],
+        origin: [
+            "http://localhost:5173",
+            "https://hotel-booking-60b59.web.app",
+            "https://hotel-booking-60b59.firebaseapp.com",
+        ],
         credentials: true,
     })
 );
