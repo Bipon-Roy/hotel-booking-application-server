7e24d6059a886661844dd33335462d4f09052403
Bipon Roy <biponroy400@gmail.com>
2024-07-16 21:54:42 +0600

    updated Review Schema

--- models/Review.js
+++ models/Review.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
 
 const ReviewSchema = new mongoose.Schema({
+    customerName: { type: String, required: true },
+    review: { type: String, required: true },
+    rating: { type: Number, required: true },
+    timestamp: { type: String, required: true },
+    userPhoto: { type: String, required: true },
     room_id: { type: String, required: true },
-    review: String,
-    rating: Number,
 });
 
 module.exports = mongoose.model("Reviews", ReviewSchema);
